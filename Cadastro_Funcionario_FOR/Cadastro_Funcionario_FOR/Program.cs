cea78b2d4907823906e879c0355a1b8faaa7fe6f
Erick Adriano Nunes da Silva <61701606+ErickNunes97@users.noreply.github.com>
2022-03-05 07:48:54 -0300

    Add files via upload

---
+++ Cadastro_Funcionario_FOR/Cadastro_Funcionario_FOR/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Cadastro_Funcionario_FOR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o número de funcionários:\n");
+            int NumFunc=int.Parse(Console.ReadLine());
+
+            for(int i =0; NumFunc>=i; i++)
+            {
+                Console.WriteLine("\nNome: ");
+                string nome = Console.ReadLine();
+                Console.WriteLine("\nIdade: ");
+                int idade = int.Parse(Console.ReadLine());
+                Console.WriteLine("\nSexo: ");
+                char s = char.Parse(Console.ReadLine());
+
+                if(s=='M' || s == 'm')
+                {
+                    Console.WriteLine("Contratado para estoque");
+                }
+
+                else
+                {
+                    Console.WriteLine("Contratada para caixa");
+                }
+
+            }
+        }
+    }
+}
